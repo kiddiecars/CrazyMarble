e321d1028cb35797a93bb29e2a0369fc965c6f9f
kiddiecars <valikizilbash@gmail.com>
2023-02-22 16:04:59 -0600

    finishing movemement stuff
    finishing movement stuff

--- Crazy Marble Project/Assets/Scripts/MarbleRotationController.cs
+++ Crazy Marble Project/Assets/Scripts/MarbleRotationController.cs
@@ -1,14 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
 
 public class MarbleRotationController : MonoBehaviour
 {
     [field: SerializeField]
- public Vector3 RotationVector { get; private set; }
+    public Vector3 RotationVector { get; private set; }
 
- void FixedUpdate() {
-Rigidbody body = GetComponent<Rigidbody>();
-body.AddTorque(RotationVector, ForceMode.VelocityChange);
- }
+    private MarbleControls _marbleControls;
+
+    void Awake() {
+        Debug.Log("I'm awake!");
+        _marbleControls = new MarbleControls();
+        _marbleControls.RotationControl.Enable();
+        _marbleControls.RotationControl.Rotate.performed += HandleRotateControl;
+        _marbleControls.RotationControl.Rotate.canceled += HandleRotateStop;
+    }
+
+    void FixedUpdate() {
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.AddTorque(RotationVector, ForceMode.VelocityChange);
+    }
+
+    private void HandleRotateControl(CallbackContext context)
+    {
+        Vector2 rawInput = context.ReadValue<Vector2>();
+        RotationVector = new Vector3(rawInput.x, 0, -rawInput.y);
+    }
+
+    private void HandleRotateStop(CallbackContext context)
+    {
+        RotationVector = Vector3.zero;
+    }
+
 }
